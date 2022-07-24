d21992e85d3406dfc0fe89a82205e54965c8541d
Andrew Wilder <andrew.m.wilder@gmail.com>
2022-07-24 04:20:39 -0700

    Fix spacing

--- WL4EditorCore/Util/ConstantExtensions.cs
+++ WL4EditorCore/Util/ConstantExtensions.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Immutable;
-using WL4EditorCore.Exception;
+﻿using WL4EditorCore.Exception;
 using static WL4EditorCore.Util.Constants;
 
 namespace WL4EditorCore.Util
@@ -11,13 +10,13 @@ namespace WL4EditorCore.Util
         {
             switch (a)
             {
-                case LayerScrollingType.NoScrolling: return "No scrolling";
-                case LayerScrollingType.HSpeedHalf: return "H speed ½ of BG1, V fixed";
-                case LayerScrollingType.VSpeedHalf: return "V speed ½ of BG1, H fixed";
-                case LayerScrollingType.HVSpeedHalf: return "H and V speed ½ of BG1";
-                case LayerScrollingType.HSyncVSpeedHalf: return "H synced wih BG1, V speed ½ of BG1";
-                case LayerScrollingType.VSyncHSpeedHalf: return "V synced wih BG1, H speed ½ of BG1";
-                case LayerScrollingType.HVSync: return "H and V synced wih BG1";
+                case LayerScrollingType.NoScrolling:       return "No scrolling";
+                case LayerScrollingType.HSpeedHalf:        return "H speed ½ of BG1, V fixed";
+                case LayerScrollingType.VSpeedHalf:        return "V speed ½ of BG1, H fixed";
+                case LayerScrollingType.HVSpeedHalf:       return "H and V speed ½ of BG1";
+                case LayerScrollingType.HSyncVSpeedHalf:   return "H synced wih BG1, V speed ½ of BG1";
+                case LayerScrollingType.VSyncHSpeedHalf:   return "V synced wih BG1, H speed ½ of BG1";
+                case LayerScrollingType.HVSync:            return "H and V synced wih BG1";
                 case LayerScrollingType.TopHalfAutoscroll: return "Upper half autoscroll to the left (⅛ speed of BG1)";
             }
             throw new InternalException("Invalid enum passed to extension method WL4EditorCore.Util.ConstantExtensions.ToString(this LayerScrollingType)");
@@ -27,16 +26,16 @@ namespace WL4EditorCore.Util
         {
             switch (a)
             {
-                case LayerSpecialEffect.NoEffect: return "No raster type effect";
-                case LayerSpecialEffect.Layer3WaterEffect1: return "Layer 3 water effect 1";
-                case LayerSpecialEffect.Layer3WaterEffect2: return "Layer 3 water effect 2";
-                case LayerSpecialEffect.Layer0FogEffect: return "Layer 0 fog effect";
-                case LayerSpecialEffect.Layer3FireEffect1: return "Layer 3 fire effect 1";
-                case LayerSpecialEffect.Layer3FireEffect2: return "Layer 3 fire effect 2";
+                case LayerSpecialEffect.NoEffect:            return "No raster type effect";
+                case LayerSpecialEffect.Layer3WaterEffect1:  return "Layer 3 water effect 1";
+                case LayerSpecialEffect.Layer3WaterEffect2:  return "Layer 3 water effect 2";
+                case LayerSpecialEffect.Layer0FogEffect:     return "Layer 0 fog effect";
+                case LayerSpecialEffect.Layer3FireEffect1:   return "Layer 3 fire effect 1";
+                case LayerSpecialEffect.Layer3FireEffect2:   return "Layer 3 fire effect 2";
                 case LayerSpecialEffect.Layer3HDMAUpperHalf: return "Layer 3 top half ⅛ scroll, bottom half fixed";
                 case LayerSpecialEffect.Layer3HDMALowerHalf: return "Layer 3 bottom half ⅛ scroll, top half fixed";
-                case LayerSpecialEffect.AlphaFireEffect1: return "Alpha fire effect 1";
-                case LayerSpecialEffect.AlphaFireEffect2: return "Alpha fire effect 2";
+                case LayerSpecialEffect.AlphaFireEffect1:    return "Alpha fire effect 1";
+                case LayerSpecialEffect.AlphaFireEffect2:    return "Alpha fire effect 2";
             }
             throw new InternalException("Invalid enum passed to extension method WL4EditorCore.Util.ConstantExtensions.ToString(this LayerSpecialEffect)");
         }
@@ -45,8 +44,8 @@ namespace WL4EditorCore.Util
         {
             switch (a)
             {
-                case LayerPriority.Order_0123: return "L0 (Top) > L1 > L2 > L3";
-                case LayerPriority.Order_1023: return "L1 (Top) > L0 > L2 > L3";
+                case LayerPriority.Order_0123:    return "L0 (Top) > L1 > L2 > L3";
+                case LayerPriority.Order_1023:    return "L1 (Top) > L0 > L2 > L3";
                 case LayerPriority.Order_1023_SP: return "L1 (Top) > L0 > L2 > L3, Sprite Priority+";
                 case LayerPriority.Order_1203_SP: return "L1 (Top) > L2 > L0 > L3, Sprite Priority+";
             }
@@ -78,17 +77,17 @@ namespace WL4EditorCore.Util
             switch (a)
             {
                 case AlphaBlending.NoAlphaBlending: return "No Alpha Blending";
-                case AlphaBlending.EVA7_EVB16: return "EVA 44%, EVB 100%";
-                case AlphaBlending.EVA10_EVB16: return "EVA 63%, EVB 100%";
-                case AlphaBlending.EVA13_EVB16: return "EVA 81%, EVB 100%";
-                case AlphaBlending.EVA16_EVB16: return "EVA 100%, EVB 100%";
-                case AlphaBlending.EVA16_EVB0: return "EVA 100%, EVB 0%";
-                case AlphaBlending.EVA13_EVB3: return "EVA 81%, EVB 19%";
-                case AlphaBlending.EVA10_EVB6: return "EVA 63%, EVB 37%";
-                case AlphaBlending.EVA7_EVB9: return "EVA 44%, EVB 56%";
-                case AlphaBlending.EVA5_EVB11: return "EVA 31%, EVB 68%";
-                case AlphaBlending.EVA3_EVB13: return "EVA 19%, EVB 81%";
-                case AlphaBlending.EVA0_EVB16: return "EVA 00%, EVB 100%";
+                case AlphaBlending.EVA7_EVB16:      return "EVA 44%, EVB 100%";
+                case AlphaBlending.EVA10_EVB16:     return "EVA 63%, EVB 100%";
+                case AlphaBlending.EVA13_EVB16:     return "EVA 81%, EVB 100%";
+                case AlphaBlending.EVA16_EVB16:     return "EVA 100%, EVB 100%";
+                case AlphaBlending.EVA16_EVB0:      return "EVA 100%, EVB 0%";
+                case AlphaBlending.EVA13_EVB3:      return "EVA 81%, EVB 19%";
+                case AlphaBlending.EVA10_EVB6:      return "EVA 63%, EVB 37%";
+                case AlphaBlending.EVA7_EVB9:       return "EVA 44%, EVB 56%";
+                case AlphaBlending.EVA5_EVB11:      return "EVA 31%, EVB 68%";
+                case AlphaBlending.EVA3_EVB13:      return "EVA 19%, EVB 81%";
+                case AlphaBlending.EVA0_EVB16:      return "EVA 00%, EVB 100%";
             }
             throw new InternalException("Invalid enum passed to extension method WL4EditorCore.Util.ConstantExtensions.ToString(this AlphaBlending)");
         }
